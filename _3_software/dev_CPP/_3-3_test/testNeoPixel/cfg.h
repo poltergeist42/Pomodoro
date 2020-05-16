191c69e45659ee95328883bc54a4954105d74c5c
poltergeist42 <lepoltergeist@gmail.com>
2020-05-16 19:29:28 +0200

    rebuilt docs

--- _3_software/dev_CPP/_3-3_test/testNeoPixel/cfg.h
+++
@@ -1,5 +0,0 @@
-// Définission de la broche de l'anneau de LED NeoPixel
-// GPIO 0 = D3 sur le NodeMCU
-#define NEOPIX_PIN 0
-#define NEOPIX_NUMPIXELS 12
-
--- _3_software/dev_Python/_3-1_test/neopix/main.py
+++
@@ -1,4 +0,0 @@
-import fxdemo
-
-# Exemple piqu茅 ici :
-# https://github.com/mchobby/esp8266-upy/tree/master/neopixel
