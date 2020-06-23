e795dae61f4041712728dad1df2f67e50453459a
Poltergeist42 <lepoltergeist@gmail.com>
2020-06-24 01:55:04 +0200

    refactoring 'NavBar'

--- _3_software/UI_WEB/src/component/Navbar.js
+++ _3_software/UI_WEB/src/component/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
+import { Navbar as BNavbar, Nav } from "react-bootstrap"
 
 
 export default class Navbar extends Component {
@@ -11,28 +12,33 @@ export default class Navbar extends Component {
             navIcon = "https://img.icons8.com/color/48/000000/tomato.png"
         }
         return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-warning">
-                {/* <span className="navbar-brand h1">Pomodoro</span> */}
-                <Link className="navbar-brand" to="/">
-                    <img src={navIcon} alt={this.props.activeItem}/>
-                </Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <Link className={`nav-link ${this.props.activeItem === "lampe" && "text-primary"}`} to="/lampe">Lampe</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className={`nav-link ${this.props.activeItem === "pomodoro" && "text-primary"}`} to="/pomodoro">Pomodoro</Link>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="https://poltergeist42.github.io/Pomodoro/">Documentation</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+            <BNavbar collapseOnSelect expand="lg" bg="warning" >
+                <BNavbar.Brand>
+                    <Link className="navbar-brand" to="/">
+                        <img src={navIcon} alt={this.props.activeItem} />
+                    </Link>
+                    {/* <span className="navbar-brand h1">Pomodoro</span> */}
+                </BNavbar.Brand>
+                <BNavbar.Brand>
+                    <h2 className="d-none d-lg-block">Pomodoro</h2>
+                </BNavbar.Brand>
+                <BNavbar.Toggle aria-controls="responsive-navbar-nav" />
+                <BNavbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav as="ul">
+                            <Nav.Item as="li">
+                                <Link className={`nav-link ${this.props.activeItem === "lampe" && "text-primary"}`} to="/lampe">Lampe</Link>
+                            </Nav.Item>
+                            <Nav.Item as="li">
+                                <Link className={`nav-link ${this.props.activeItem === "pomodoro" && "text-primary"}`} to="/pomodoro">Pomodoro</Link>
+                            </Nav.Item>
+                            <Nav.Item as="li">
+                                <a className="nav-link" href="https://poltergeist42.github.io/Pomodoro/">Documentation</a>
+                            </Nav.Item>
+                        </Nav>
+                    </Nav>
+                </BNavbar.Collapse>
+            </BNavbar>
         )
     }
 }
